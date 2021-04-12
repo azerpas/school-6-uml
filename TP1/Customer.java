76a0e71e0e3c7f6be9b51bdc0ac55564d91ed1f6
azerpas <xskylix@hotmail.fr>
2021-04-12 08:37:37 +0200

    first commit

---
+++ TP1/Customer.java
@@ -0,0 +1,7 @@
+package TP1;
+
+public class Customer {
+    private int CRM_ID;
+    private CustomerType type;
+    private String description;
+}
---
+++ TP1/CustomerType.java
@@ -0,0 +1,5 @@
+package TP1;
+
+public enum CustomerType {
+    Individual, Company
+}
