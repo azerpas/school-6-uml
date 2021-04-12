a019ff1fcf6333ad2e38df1497f8bb53ce360cda
azerpas <xskylix@hotmail.fr>
2021-04-12 09:22:39 +0200

    Some getters

--- TP1/BatchCode.java
+++ TP1/BatchCode.java
@@ -6,4 +6,32 @@ public class BatchCode {
     private int id;
     private Set<Product> products;
     private Set<Feature> features;
+
+    public BatchCode(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Set<Product> getProducts() {
+        return this.products;
+    }
+
+    public void setProducts(Set<Product> products) {
+        this.products = products;
+    }
+
+    public Set<Feature> getFeatures() {
+        return this.features;
+    }
+
+    public void setFeatures(Set<Feature> features) {
+        this.features = features;
+    }
 }
--- TP1/Feature.java
+++ TP1/Feature.java
@@ -1,5 +1,37 @@
 package TP1;
 
 public class Feature {
-    
+    private int id;
+    private String name;
+    private String description;
+
+    public Feature(int id, String name, String description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
