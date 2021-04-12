82464113caa08e22f187ed7d69976143cf4c4c82
azerpas <xskylix@hotmail.fr>
2021-04-12 08:52:02 +0200

    Contacts

---
+++ TP1/Contact.java
@@ -0,0 +1,9 @@
+package TP1;
+
+public class Contact {
+    private String firstName;
+    private String lastName;
+    private String middleName;
+    private String email;
+    private String locale;
+}
---
+++ TP1/Details.java
@@ -0,0 +1,8 @@
+package TP1;
+
+public class Details {
+    private String street;
+    private String city;
+    private String postalCode;
+    private String country;
+}
